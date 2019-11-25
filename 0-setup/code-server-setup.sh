3b5975ade1dc6f588ea79d3fee9d3016e109fad9
EC2 Default User <ec2-user@ip-172-31-41-146.ec2.internal>
2019-11-25 08:31:54 +0000

    fix url of download

--- 0-setup/code-server-setup.sh
+++ 0-setup/code-server-setup.sh
@@ -5,8 +5,8 @@ echo "http://$ip:8443"
 security_group=$(ec2-metadata -s | cut -d " " -f 2);
 aws ec2 authorize-security-group-ingress --group-name $security_group --protocol tcp --port 8443 --cidr 0.0.0.0/0
 cs_version="2.1692-vsc1.39.2"
-wget https://github.com/codercom/code-server/releases/download/$cs_version/code-server$cs_version-linux-x64.tar.gz
-tar -xvzf code-server$cs_version-linux-x64.tar.gz
-cd code-server$cs_version-linux-x64
+wget https://github.com/codercom/code-server/releases/download/$cs_version/code-server$cs_version-linux-x86_64.tar.gz
+tar -xvzf code-server$cs_version-linux-x86_64.tar.gz
+cd code-server$cs_version-linux-x86_64
 chmod +x code-server
 ./code-server -p 8443
